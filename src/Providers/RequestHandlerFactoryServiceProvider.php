197c2edac54beb08caba6d5f98f69d44ed9c7fa9
Kenji Nakae <nakae@nextat.co.jp>
2017-12-06 14:29:52 +0900

    change RequestHandler

--- src/Providers/RequestHandlerFactoryServiceProvider.php
+++ src/Providers/RequestHandlerFactoryServiceProvider.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 namespace N1215\Tsukuyomi\Providers;
 
 use Illuminate\Container\Container;
-use N1215\Jugoya\RequestHandlerFactory;
+use N1215\Jugoya\RequestHandlerFactoryInterface;
+use N1215\Jugoya\LazyRequestHandlerFactory;
 
 class RequestHandlerFactoryServiceProvider
 {
     public function register(Container $container)
     {
-        $container->singleton(RequestHandlerFactory::class, function (Container $container) {
-            return RequestHandlerFactory::fromContainer($container);
+        $container->singleton(RequestHandlerFactoryInterface::class, function (Container $container) {
+            return LazyRequestHandlerFactory::fromContainer($container);
         });
     }
 }
